85b42495d55c2a8bfa89d2619bdd07f38714bcb4
Michael Klein <miketheklein@gmail.com>
2017-12-15 10:00:23 -0800

    more working algorithms, werk

--- tripleDigits.js
+++ tripleDigits.js
@@ -1,19 +1,24 @@
+
 function tripledouble(num1, num2) {
-  let result1 = (/([0-9])\1{2,}/).test(num1)
-  let result2 = (/([0-9])\1{1,}/).test(num2)
+  let result1 = (/([0-9])\1{2,}/gi).test(num1)
+  let result2 = (/([0-9])\1{1,}/gi).test(num2)
   console.log(num1, num2)
 
-  let repeat1 = num1.toString().match(/([0-9])\1{2,}/)
-  if (result1) {
-    console.log('repeat1',repeat1[0])
-    let repeat2 = num2.toString().match(/([0-9])\1{1,}/)
-    console.log('repeat2',repeat2)
-
-    let sameNumber = num2.toString().match(repeat1[1].repeat(2))
+  let repeat1 = num1.toString().match(/([0-9])\1{2,}/gi)
+  if (result1 && result2) {
+    console.log('repeat1', repeat1)
+    let repeat2 = num2.toString().match(/([0-9])\1{1,}/gi)
+    console.log('repeat2', repeat2)
+    console.log('repeat1.join (╯°□°)╯︵ ┻━┻ ', repeat1.join('').split(''))
+    console.log('repeat1.join (╯°□°)╯︵ ┻━┻ ', repeat2.join('').split(''))
+    let sameNumber = repeat2.join('').split('').some(num => repeat1.join('').split('').includes(num.toString()))
+    console.log('sameNumber (╯°□°)╯︵ ┻━┻ ', sameNumber)
     if (result1 && result2 && sameNumber) {
-      return 1 
-    } else return 0
-  }
+      return 1
+    } else {
+      return 0
+    }
+  } return 0
 }
 
 tripledouble(451999277, 41177722899)
